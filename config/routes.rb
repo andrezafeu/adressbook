2f248a4fdad414cd6e28c386be09d26091fc3714
Andreza Feu <andrezafeu@gmail.com>
2016-04-19 17:17:31 -0400

    Create route to show page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get '/contacts/new', to: 'contacts#new', as: :new
 
   post '/contacts', to: 'contacts#create'
+
+  get '/contacts/show', to 'contacts#show'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
